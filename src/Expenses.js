61cd718022359487b18ffc605b84e106166b3897
Addie <azm16@macbookpro-73f6.up.corp.upc>
2017-03-07 09:43:58 -0500

    graph

--- src/Expenses.js
+++ src/Expenses.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Link } from 'react-router';
 import Graph from './Graph';
 
-let data = {
-        labels: ["Red", "Blue", "Yellow"],
-        datasets: [{
-            label: '# of Votes',
-            data: [100, 30, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-            ],
-            borderWidth: 1
-        }]
-    }
+// let data = {
+//         labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
+//         datasets: [{
+//             label: 'LTD Calculator',
+//             data: [100, 30, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//             ],
+//             borderWidth: 1
+//         }]
+//     }
 
 export default class Expenses extends React.Component {
     render() {
@@ -28,7 +28,8 @@ export default class Expenses extends React.Component {
                 <h1>Disability Calculator</h1>
                 <p>Find out what amount of coverage is right for you.</p>
                 <h2>What are your monthly expenses?</h2>
-                <Graph chartData={data}/>
+                <input className="input"/><button className="button">Clicky</button>
+                <Graph chartData={this.props.chartData}/>
                 <ul role="navigation">
                     <li><Link to="/">Previous</Link></li>
                     <li><Link to="/results">Next</Link></li>
--- src/Graph.js
+++ src/Graph.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { Bar } from 'react-chartjs';
-// var Bar = require('react-chartjs');
-// var Bar = require('react-chartjs').Bar;
+// import './Expenses';
 
 export default class Graph extends React.Component {
     constructor(props) {
         super(props);
-        // this.data = this.props.data;
+        this.state = {
+            chartData: {
+                labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
+                datasets: [{
+                    data: [100, 80, 20],
+                    fillColor: ["#2bb6d9", "#8dbf48", "#f2c849"]
+                }]
+            },
+            options: {
+                scales: {
+                    // yAxes: [{
+                    //     stacked: true
+                    // }],
+                    // xAxes: [{
+                    //     stacked: true
+                    // }]
+                }
+            }
+        };
     }
 
     render() {
         return (
-            <Bar data={ this.props.chartData } width="500" height="500"/>
+            <Bar data={ this.state.chartData } options={ this.state.options } width="500" height="500"/>
         )
     }
 };
