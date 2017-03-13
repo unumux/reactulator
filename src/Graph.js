0f57de3bc20b5ba861f62fc51b296b1fd6fb3ece
Kyle Wagner <Keale2@gmail.com>
2017-03-13 15:58:53 -0400

    Update graph

--- src/Graph.js
+++ src/Graph.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
-export default class Graph extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const chartData = {
+        labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
+        datasets: [{
+            data: [40, 50, 60],
+            backgroundColor: ["#2bb6d9", "#8dbf48", "#f2c849"]
+        },
+        {
+            data: [
+                0, 10, 20
+            ],
+            backgroundColor: ["red", "blue", "green"]
+        }],
+    };
 
-    render() {
-        const chartData = {
-                labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
-                datasets: [{
-                    data: [40, 50, 60],
-                    backgroundColor: ["#2bb6d9", "#8dbf48", "#f2c849"]
-                },
-                {
-                    data: [
-                        0, 10, 20
-                    ],
-                    backgroundColor: ["red", "blue", "green"]
-                }],
-            };
-
-            const chartOptions = {
-                scales: {
-                    xAxes: [{
-                        stacked: true
-                    }],
-                    yAxes: [{
-                        stacked: true
-                    }]
-                }
-            };
-
-
-        return (
-            <Bar data={ chartData } options={ chartOptions } width={500} height={500}/>
-        )
+const chartOptions = {
+    scales: {
+        xAxes: [{
+            stacked: true
+        }],
+        yAxes: [{
+            stacked: true
+        }]
     }
 };
+
+
+const Graph = ({monthlyIncome, additionalIncome, currentCoverage, unumCoverage, totalExpenses}) => {
+    chartData.datasets[0].data = [
+        monthlyIncome,
+        additionalIncome,
+        totalExpenses
+    ];
+
+    return (
+        <Bar data={ chartData } options={ chartOptions } width={500} height={500}/>
+    );
+};
+
+export default Graph;
