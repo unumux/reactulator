2e350a5f57bd48adc48538acc5f30348da49a267
Kyle Wagner <Keale2@gmail.com>
2017-03-13 16:46:22 -0400

    Updates graph

--- src/Graph.js
+++ src/Graph.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
+
 const chartData = {
-        labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
-        datasets: [{
-            data: [40, 50, 60],
-            backgroundColor: ["#2bb6d9", "#8dbf48", "#f2c849"]
-        },
-        {
-            data: [
-                0, 10, 20
-            ],
-            backgroundColor: ["red", "blue", "green"]
-        }],
-    };
+    labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
+    datasets: [{
+        backgroundColor: ["#2bb6d9", "#8dbf48", "#f2c849"]
+    },
+    {
+        backgroundColor: ["#0b96b9", "#6d9f28", "#E48E43"]
+    },
+    {
+        backgroundColor: ["transparent", "#4d7f08", "transparent"]
+    }]
+};
 
 const chartOptions = {
     scales: {
@@ -26,13 +26,16 @@ const chartOptions = {
     }
 };
 
-
 const Graph = ({monthlyIncome, additionalIncome, currentCoverage, unumCoverage, totalExpenses}) => {
-    chartData.datasets[0].data = [
-        monthlyIncome,
-        additionalIncome,
-        totalExpenses
-    ];
+    const layer1 = [];
+    const layer2 = [];
+    const layer3 = [];
+
+    // PUT LOGIC HERE
+
+    chartData.datasets[0].data = layer1;
+    chartData.datasets[1].data = layer2;
+    chartData.datasets[2].data = layer3;
 
     return (
         <Bar data={ chartData } options={ chartOptions } width={500} height={500}/>
