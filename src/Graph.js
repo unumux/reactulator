255fb921172a4ce0ade754a8d3a77633f6e4c2dc
Addie <azm16@macbookpro-73f6.up.corp.upc>
2017-03-15 10:21:50 -0400

    dummy data for graph, colors

--- src/Graph.js
+++ src/Graph.js
@@ -11,23 +11,35 @@ export default class Graph extends React.Component {
                 labels: ["Monthly Income when healthy", "Monthly Income with disability", "Expenses"],
                 datasets: [{
                     data: [40, 50, 60],
-                    backgroundColor: ["#2bb6d9", "#8dbf48", "#f2c849"]
+                    backgroundColor: ["#2bb6d9", "#81b245", "#f2c849"]
                 },
                 {
+                    label: ["Current Coverage"],
                     data: [
-                        0, 10, 20
+                        0, 10, 0
                     ],
-                    backgroundColor: ["red", "blue", "green"]
-                }],
+                    backgroundColor: ["transparent", "#9cca62", "transparent"]
+                },
+                {
+                    label: ["Unprotected Expenses"],
+                    data: [
+                        0, 0, 30
+                    ],
+                    backgroundColor: ["transparent", "transparent", "#f8a346"]
+                },
+                {
+                    label: ["Unum Coverage"],
+                    data: [
+                        0, 30, 0
+                    ],
+                    backgroundColor: ["transparent", "#004276", "transparent"]
+                }]
             };
 
             const chartOptions = {
                 scales: {
-                    xAxes: [{
-                        stacked: true
-                    }],
                     yAxes: [{
-                        stacked: true
+                        stacked: true,
                     }]
                 }
             };
